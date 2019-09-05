40429f07f90dfeb1ce27347598cbb9bc87d9b38e
kolkol <kolodiy1103@gmail.com>
2019-09-06 01:52:51 +0200

    Initial Commit

---
+++ Eight-Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Eight-Ball
+//
+//  Created by Maksym Kolodiy on 06/09/2019.
+//  Copyright © 2019 Maksym Kolodiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
