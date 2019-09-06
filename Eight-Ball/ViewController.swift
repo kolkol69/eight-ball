8d33b20c2aa31970d6722b63502dde40e87900dd
kolkol <kolodiy1103@gmail.com>
2019-09-06 02:13:23 +0200

    Create app that answers your questions xd  todo: add some response on ball change so you can see that the asnwer is new in case of the two in a row are the same

--- Eight-Ball/ViewController.swift
+++ Eight-Ball/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ballImage: UIImageView!
+    var randomNumber : Int = 0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        changeBallImageAnswer()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        changeBallImageAnswer();
+    }
+    
+    func changeBallImageAnswer(){
+        randomNumber = Int(arc4random_uniform(5)) + 1
+        
+        ballImage.image = UIImage(named: "ball\(randomNumber)")
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        changeBallImageAnswer();
+    }
+    
 }
 
